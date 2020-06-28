3ee39166fb5ab73040c710acedb10d22a10ab690
Nitish Gupta <imnitish.ng@gmail.com>
2020-06-28 14:10:50 +0530

    part1d: Add exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,18 +6,41 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allclicks, setAll] = useState(0)
+  const [average, setAvg] = useState(0)
+
+  const handleGood = () => {
+    setAvg(average+1)
+    setAll(allclicks+1)
+    setGood(good+1)
+  }
+
+  const handleNeutral = () => {
+    setAvg(average+0)
+    setAll(allclicks+1)
+    setNeutral(neutral+1)
+  }
+
+  const handleBad = () => {
+    setAvg(average-1)
+    setAll(allclicks+1)
+    setBad(bad+1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good+1)}>Good</button>
-      <button onClick={() => setNeutral(neutral+1)}>Neutral</button>
-      <button onClick={() => setBad(bad+1)}>Bad</button>
+      <button onClick={handleGood}>Good</button>
+      <button onClick={handleNeutral}>Neutral</button>
+      <button onClick={handleBad}>Bad</button>
       <h1>statistics</h1>
       <div>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {allclicks}</p>
+        <p>average {average/allclicks}</p>
+        <p>positive {good/allclicks*100}%</p>
       </div>
     </div>
   )
