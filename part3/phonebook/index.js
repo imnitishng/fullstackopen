929f520c07c9523f6a60bc6ddd40d5625ed279a7
Nitish Gupta <imnitish.ng@gmail.com>
2020-10-14 23:03:29 +0530

    part3a: Add exercise 3.1 to 3.5

---
+++ part3/phonebook/index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const { response, request } = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-123456"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "040-344322"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(
+    `Phonebook has info for ${persons.length} people\n\n${new Date()}`
+  )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const personID = Number(request.params.id)
+  const person = persons.find(person => person.id === personID)
+  if(person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
+const generateID = () => (
+  Math.floor(Math.random()*1000)
+)
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(request.body)
+  if(!body.number) {
+    response.status(400).json({
+      error: "missing number"
+    })
+  }
+  else if(!body.name) {
+    response.status(400).json({
+      error: "missing name"
+    })
+  }
+  else {
+    const nameAlreadyExist = persons.find(person => person.name === body.name)
+    if(nameAlreadyExist) {
+      response.status(400).json({
+        error: "name must be unique"
+      })
+    }
+    else {
+      const person = {
+        id: generateID(),
+        name: body.name,
+        number: body.number
+      }
+      persons = persons.concat(person)
+      response.json(person)
+    }
+  }
+})
+
+const PORT = 3301
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
