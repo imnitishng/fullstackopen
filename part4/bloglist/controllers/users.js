b6924161c366c2442922aa675d1bcd4672dd53e5
Nitish Gupta <imnitish.ng@gmail.com>
2020-11-03 22:41:42 +0530

    part4d: Add exercise 4.15

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/users')
+
+usersRouter.post('/', async (request, response, next) => {
+  try {
+    console.log(request.body)
+    const body = request.body
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    })
+    const savedUser = await user.save()
+    response.json(savedUser)
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get('/', async (request, response, next) => {
+  try {
+    const users = await User.find({})
+    response.json(users)
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+module.exports = usersRouter
